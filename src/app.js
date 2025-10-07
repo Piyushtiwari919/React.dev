e5be2673dd9193ee551b9709d4a46844f96a3e55
Piyush Tiwari <piyushtiwari919921@gmail.com>
2025-10-07 21:42:38 +0530

    Eatables

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import Header from "./components/Header.js";
 import Body from "./components/Body.js";
 import Footer from "./components/Footer.js";
 
+
+
 const AppLayout = () => {
   console.log(<Body/>);
   
