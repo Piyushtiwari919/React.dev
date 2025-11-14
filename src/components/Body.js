8c8546d79d1c9a3fd4e67ba4b50036f0a14eee01
Piyush Tiwari <piyushtiwari919921@gmail.com>
2025-11-14 19:13:52 +0530

    Updating Body of the Web App

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,20 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 // import { Res_List } from "../utils/mockData.js";
-import RestaurantCard from "./RestaurantCard.js";
+import RestaurantCard, { IsOpenLabel } from "./RestaurantCard.js";
 import { Link } from "react-router-dom";
 import SearchBox from "./SearchBox.js";
 import Shimmer from "./Shimmer.js";
-import useOnlineStatus from "../utils/useOnlineStatus.js";
+import UserContext from "../utils/UserContext.js";
+import useOnlineStatus from "../../src/utils/useOnlineStatus.js";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurant] = useState([]);
   const [filteredRestaurant, setFilteredRestaurant] = useState([]);
   const [searchText, setSearchText] = useState("");
+
+  const OpenRestaurantCard = IsOpenLabel(RestaurantCard);
+  const {loggedInUser,setUserName} = useContext(UserContext);
+
   useEffect(() => {
     fetchData();
   }, []);
@@ -21,18 +26,18 @@ const Body = () => {
       );
       // console.log(data);
       const text = await data.text();
-      console.log(text);
+      // console.log(text);
 
       const json = JSON.parse(text);
-      console.log(json);
-      const cardObj = json?.data?.cards.find((item) =>
-        item.card?.card.id.includes("top_brands_for_you")
+      // console.log(json);
+      const cardObj = json?.data?.cards.find(
+        (item) => item.card?.card.id.includes("top_brands_for_you")
         // item.card?.card.id.includes("restaurant_grid_listing")
       );
-      console.log(cardObj);
+      // console.log(cardObj);
       const finalRes =
         cardObj?.card?.card?.gridElements?.infoWithStyle?.restaurants;
-      console.log(finalRes);
+      // console.log(finalRes);
 
       setListOfRestaurant(finalRes);
       setFilteredRestaurant(finalRes);
@@ -62,15 +67,15 @@ const Body = () => {
   // }
   */
 
-  console.log("abc first");
+  // console.log("abc first");
 
   return listOfRestaurants.length === 0 ? (
     <Shimmer />
   ) : (
-    <div className="">
+    <div className="flex text-center flex-col m-2">
       <div className="search-container">
         {/* <SearchBox /> */}
-        <div className="search-box">
+        <div className="search-box mx-2 my-6 p-2 text-md rounded-2xl flex items-center border-amber-800 border-2 w-70 hover:border-blue-800">
           <input
             type="text"
             placeholder="What are you looking for?"
@@ -79,46 +84,61 @@ const Body = () => {
               setSearchText(e.target.value);
             }}
             name="dishes-search"
+            className="m-1 p-1 w-52 outline-none text-cyan-950 font-medium bg-white"
           />
           <button
             onClick={() => {
-              const filteredData = listOfRestaurants.filter((res) => {
-                return res.info.name
-                  .toLowerCase()
-                  .includes(searchText.toLowerCase());
-              });
+              if (searchText !== "") {
+                const filteredData = listOfRestaurants.filter((res) => {
+                  return res.info.name
+                    .toLowerCase()
+                    .includes(searchText.toLowerCase());
+                });
+                setFilteredRestaurant(filteredData);
+              }
               // console.log(filteredData);
-
-              setFilteredRestaurant(filteredData);
             }}
+            className="hover:cursor-pointer"
           >
-            <i className="fa-solid fa-magnifying-glass"></i>
+            <i className="fa-solid fa-magnifying-glass text-lg hover:text-pink-500"></i>
           </button>
         </div>
+        <div className="search-box mx-2 my-6 p-2 text-md rounded-2xl flex items-center border-orange-400 border-2 w-70 hover:border-blue-800">
+          <input
+            type="text"
+            value={loggedInUser}
+            onChange={(e)=>setUserName(e.target.value)}
+            className="m-1 p-1 w-52 outline-none text-fuchsia-900 font-medium bg-white"
+          />
+        </div>
       </div>
-      <div className="filter-box">
+      <div className="filter-box mx-2 my-2">
         <button
-          className="filter-btn"
+          className="filter-btn border-cyan-700 border-2 cursor-pointer rounded-md p-2 hover:bg-amber-800 hover:text-white"
           onClick={() => {
             const filteredList = listOfRestaurants.filter((resD) => {
               return resD.info.avgRating > 4.3;
             });
             setFilteredRestaurant(filteredList);
-            console.log(filteredList);
+            // console.log(filteredList);
           }}
         >
           Top Rated Restaurants
         </button>
       </div>
-      <div className="res-container">
+      <div className="res-container flex gap-3 my-4 flex-wrap justify-center">
         {filteredRestaurant.map((resD) => {
           return (
             <Link
               to={"/restaurants/" + resD.info.id}
               key={resD.info.id}
-              className="res-links"
+              className="res-links my-2 flex flex-col items-center rounded-lg w-56 hover:bg-amber-300"
             >
-              <RestaurantCard resData={resD.info} />
+              {resD.info.isOpen ? (
+                <OpenRestaurantCard resData={resD.info} />
+              ) : (
+                <RestaurantCard resData={resD.info} />
+              )}
             </Link>
           );
         })}
